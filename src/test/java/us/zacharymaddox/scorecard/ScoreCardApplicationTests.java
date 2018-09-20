4a14a65c9078b63787ef144de07b6370c479c9f2
Zach Maddox <zmad5306@protonmail.com>
2018-09-20 17:28:00 -0400

    init project

---
+++ src/test/java/us/zacharymaddox/scorecard/ScoreCardApplicationTests.java
@@ -0,0 +1,16 @@
+package us.zacharymaddox.scorecard;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ScoreCardApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
