c38fa18455b5d2dcfad05f3085b65a9c428d7b11
Zach Maddox <zmad5306@protonmail.com>
2018-09-21 20:23:53 -0400

    added basic score card list page to portal

---
+++ src/main/java/us/zacharymaddox/scorecard/portal/ScoreCardPortalController.java
@@ -0,0 +1,31 @@
+package us.zacharymaddox.scorecard.portal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import us.zacharymaddox.scorecard.api.service.ScoreCardApiService;
+import us.zacharymaddox.scorecard.common.domain.ScoreCardStatus;
+
+@Controller
+@RequestMapping("/portal/scorecard")
+public class ScoreCardPortalController {
+	
+	@Autowired
+	private ScoreCardApiService scoreCardApiService;
+	
+	@GetMapping("/list")
+	public String list(
+			@RequestParam(name="score_card_status", required=false, defaultValue="COMPLETED") ScoreCardStatus scoreCardStatus,
+			@RequestParam(name="rows", required=false, defaultValue="10") Integer rows,
+			@RequestParam(name="page", required=false, defaultValue="0") Integer page,
+			Model model
+		) {
+		model.addAttribute("scorecards", scoreCardApiService.getScoreCards(scoreCardStatus, rows, page));
+		return "portal/scorecard/list";
+	}
+
+}
