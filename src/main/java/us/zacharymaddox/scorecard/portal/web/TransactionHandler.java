f33c19678b68c532ffa188da7d5a03942e4d9b50
Zach Maddox <zmad5306@protonmail.com>
2018-09-29 18:14:30 -0400

    more web flow for adding a new transaction

---
+++ src/main/java/us/zacharymaddox/scorecard/portal/web/TransactionHandler.java
@@ -0,0 +1,39 @@
+package us.zacharymaddox.scorecard.portal.web;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import us.zacharymaddox.scorecard.api.domain.Action;
+import us.zacharymaddox.scorecard.api.domain.Transaction;
+import us.zacharymaddox.scorecard.api.service.TransactionApiService;
+
+@Component
+public class TransactionHandler {
+	
+	@Autowired
+	private TransactionApiService transactionApiService;
+
+	public Transaction init() {
+		System.out.println("called init...");
+		Transaction transaction = new Transaction();
+		transaction.setActions(new ArrayList<>());
+		return transaction;
+	}
+	
+	public void addTransaction(Transaction transaction, Transaction formData) {
+		System.out.println("called addTransaction...");
+		transaction.setName(formData.getName());
+	}
+	
+	public void addAction(Transaction transaction, Action action) {
+		System.out.println("called addAction...");
+		transaction.getActions().add(action);
+	}
+	
+	public void save(Transaction transaction) {
+		System.out.println("called save...");
+	}
+	
+}
