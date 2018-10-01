19f18d47ff2068025824a7bfa4dc01d674405a9e
Zach Maddox <zmad5306@protonmail.com>
2018-10-01 17:34:56 -0400

    saved depends on

--- src/main/java/us/zacharymaddox/scorecard/core/domain/TransactionActionListDeserializer.java
+++ src/main/java/us/zacharymaddox/scorecard/core/domain/TransactionActionListDeserializer.java
@@ -2,8 +2,13 @@ package us.zacharymaddox.scorecard.core.domain;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Configurable;
 
@@ -34,6 +39,7 @@ public class TransactionActionListDeserializer extends StdDeserializer<List<Tran
 		Iterator<JsonNode> elements = node.elements();
 		List<TransactionAction> actions = new ArrayList<>();
 		Transaction transaction = (Transaction) p.getCurrentValue();
+		Map<Long, List<Long>> dependsOn = new HashMap<>();
 		while (elements.hasNext()) {
 			JsonNode nd = elements.next();
 			Action action = new Action();
@@ -41,10 +47,30 @@ public class TransactionActionListDeserializer extends StdDeserializer<List<Tran
 			TransactionAction ta = new TransactionAction();
 			ta.setAction(action);
 			ta.setTransaction(transaction);
-			// TODO
-			// ta.setDependsOn(dependsOn);
+			ArrayNode dpo = (ArrayNode) nd.get("depends_on");
+			dpo.forEach(s -> {
+				if (!dependsOn.containsKey(action.getActionId())) {
+					dependsOn.put(action.getActionId(), new ArrayList<>());
+				}
+				dependsOn.get(action.getActionId()).add(s.asLong());
+			});
 			actions.add(ta);
 		}
+
+		for (TransactionAction transAction : actions) {
+			Set<TransactionAction> dependsOnSet = new HashSet<>();
+			List<Long> deps = dependsOn.get(transAction.getAction().getActionId());
+			if (null != deps) {
+				for (Long depActionId : deps) {
+					Optional<TransactionAction> dep = actions.stream().filter(d -> d.getAction().getActionId() == depActionId).findFirst();
+					if (dep.isPresent()) {
+						dependsOnSet.add(dep.get());
+					}
+				}
+			}
+			transAction.setDependsOn(dependsOnSet);
+		}
+		
 		return actions;
 	}
 
